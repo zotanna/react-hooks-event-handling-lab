8c3c5870e90c20dde4cd60e6ffdbc1979e21cbec
denis <denniiskim@gmail.com>
2023-11-27 04:46:29 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handlePasswordChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handlePasswordChange} />
+    </div>
+  );
 }
 
 export default Keypad;
